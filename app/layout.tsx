a19a6b63456d8603bead0a252f0fbc07ab3cf38f
faiziatgit <faizanzafrit@gmail.com>
2026-09-22 01:15:36 +0500

    Title-Changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Areeba & Hamza | Mehndi Night",
+  title: "Mehndi",
   description: "You are invited to celebrate our Mehndi Night.",
 };
 
