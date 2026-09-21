a7f5339328071e070b9fcfab2aab3550d3703d13
faiziatgit <faizanzafrit@gmail.com>
2026-09-22 01:41:02 +0500

    Open graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-const siteUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+const siteUrl = "https://mehndi-navy.vercel.app";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
